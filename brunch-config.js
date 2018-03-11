a1e6f62f59b3a6d645403bd7f5e33a0b0127caa9
Tung Dao <me@tungdao.com>
2018-03-11 13:45:28 +0700

    static assets working

--- brunch-config.js
+++ brunch-config.js
@@ -1,5 +1,15 @@
-// See http://brunch.io for documentation.
 exports.files = {
-  javascripts: {joinTo: 'app.js'},
-  stylesheets: {joinTo: 'app.css'}
-};
+  javascripts: {
+    joinTo: 'app.js'
+  },
+
+  stylesheets: {
+    joinTo: 'app.css'
+  }
+}
+
+exports.modules = {
+  autoRequire: {
+    'app.js': ['main']
+  }
+}
--- src/App.hs
+++ src/App.hs
@@ -1,9 +1,27 @@
 module App where
 
 import RIO
-import Network.Wai
+import Network.Wai.Middleware.Static
+import Network.Wai.Handler.Warp
 import Servant
 
 
+type SVGEasyAPI = Raw
+
+
 start :: IO ()
-start = undefined
+start = do
+  options <- logOptionsHandle stdout True
+  withLogFunc options $ \lf -> runEnv 4000
+    $ staticPolicy (defaultIndex >-> addBase "public")
+    $ serve api $ enter (nt lf) app
+  where
+    api = Proxy :: Proxy SVGEasyAPI
+    nt env = NT (Servant.Handler . runRIO env)
+
+defaultIndex :: Policy
+defaultIndex = policy $ \path ->
+  Just $ if path == "" then "index.html" else path
+
+app :: (HasLogFunc env) => ServerT SVGEasyAPI (RIO env)
+app = undefined
