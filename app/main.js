10dc802a5a7c488f5dddb6bfa6f8ede2f8dc50ac
Tung Dao <me@tungdao.com>
2018-03-13 10:29:38 +0700

    turn off debug on production

--- app/main.js
+++ app/main.js
@@ -1,5 +1,13 @@
+let Ractive = require('ractive')
+
+Ractive.DEBUG = /unminified/.test(function(){/*unminified*/});
+Ractive.DEBUG_PROMISES = true
+
+
 const SVGBuilder = require('builder.html')
 const builder = new SVGBuilder({ el: '#main' })
+
+window.__builder = builder
 
 fetch(`/icon-sets`)
   .then(resp => resp.json())
